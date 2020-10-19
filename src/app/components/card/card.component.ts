f54fcca34fa90f4f8a22ca0cac32a09c0dec90d7
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-19 22:51:36 +0530

    Update

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -4,16 +4,15 @@ import { DataServiceService } from 'src/app/service/Data/data-service.service';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-
-  constructor(private service:DataServiceService) { }
-  apiData
+  constructor(private service: DataServiceService) {}
+  apiData;
 
   ngOnInit(): void {
-    this.apiData= this.service.getData()
-    // console.log( "Card", this.apiData); 
+    this.apiData = this.service.getData();
+    this.service.setData([]);
+    // console.log( "Card", this.apiData);
   }
-
 }
