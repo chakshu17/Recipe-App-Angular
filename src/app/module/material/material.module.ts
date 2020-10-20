d91ada5470b5ae9e113d828e1f82e35b89db6f87
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-20 10:15:37 +0530

    update

--- src/app/module/material/material.module.ts
+++ src/app/module/material/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
@@ -12,12 +12,9 @@ import {MatButtonModule} from '@angular/material/button';
     CommonModule,
     MatCardModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
   ],
-  exports:[
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule
-  ]
+  exports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
